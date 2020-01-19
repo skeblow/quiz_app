cea4460002ba0ca4b6f8861182e8dbfe8893367b
David Strba <david.strba@maternia.cz>
2020-01-19 17:09:47 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Server side of quiz app
+
+- https://electronjs.org/docs/tutorial/first-app
---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
